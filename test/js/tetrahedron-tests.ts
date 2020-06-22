d4dfe5f50adfa4b16d6681e008b8ec46ff61f993
John Chase <ratteal@gmail.com>
2020-06-22 13:00:49 +0100

    Allow for tests with different configuration files.

--- test/js/tetrahedron-tests.ts
+++ test/js/tetrahedron-tests.ts
@@ -1,5 +1,5 @@
-import * as valid_config from "../valid-test-puzzle-data.json";
-import * as invalid_config from "../invalid-test-puzzle-data.json";
+import * as valid_config1 from "../valid-test-puzzle-data1.json";
+import * as invalid_config1 from "../invalid-test-puzzle-data1.json";
 import { Tetrahedron } from '../../src/js/tetrahedron';
 import { expect } from 'chai';
 import 'mocha';
@@ -8,17 +8,17 @@ describe("Tetrahedron behaviour", () => {
 
     describe("a new Tetrahedron", () => {
 
-        context("with valid parameters", () => {
+        context("with valid configuration file", () => {
             it("should return a correctly initialised instance", () => {
-                const puzzle = new Tetrahedron(valid_config);
+                const puzzle = new Tetrahedron(valid_config1.testPuzzleData);
                 expect(puzzle.name).to.equal("test-valid");
             });
         });
 
-        context("with invalid parameters", () => {
+        context("with invalid configuration file 1", () => {
             it("should throw an error", () => {
                 expect(() => {
-                    new Tetrahedron(invalid_config);
+                    new Tetrahedron(invalid_config1.testPuzzleData);
                 }).to.throw(Error, "Tetrahedron must always have configuration data for 4 Faces!");
             });
         });
