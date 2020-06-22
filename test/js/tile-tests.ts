2eb6565998dd69f48f1773574d6a480fda97ad81
John Chase <ratteal@gmail.com>
2020-06-22 15:42:16 +0100

    Simple Tile test.

---
+++ test/js/tile-tests.ts
@@ -0,0 +1,27 @@
+import { Tile } from '../../src/js/tile';
+import { TileData } from "../../src/js/puzzle-data-schema";
+import { expect } from 'chai';
+import 'mocha';
+
+
+describe("Tile behaviour", () => {
+
+    describe("if a new Tile is created", () => {
+
+        context("is created with a valid identifier", () => {
+            it("should return a correctly initialised instance", () => {
+                const tileData: TileData = {
+                    "tile": "TestTile",
+                    "sideA": "SegmentData",
+                    "sideB": "SegmentData",
+                    "sideC": "SegmentData"
+                };
+                let tile = new Tile(tileData);
+                expect(tile).to.be.an.instanceOf(Tile);
+                expect(tile.id).to.equal(tileData.tile);
+            });
+        });
+
+    });
+
+});
