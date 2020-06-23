57bf693f75621474f8f6af4e9fd8440d55604f2f
John Chase <ratteal@gmail.com>
2020-06-23 16:41:44 +0100

    Test for getting random tiles from the pool.

--- test/js/tile-pool-tests.ts
+++ test/js/tile-pool-tests.ts
@@ -1,5 +1,6 @@
 import * as valid_config1 from "../valid-test-puzzle-data1.json";
 import * as invalid_config1 from "../invalid-test-puzzle-data1.json";
+import { Tile } from "../../src/js/tile";
 import { TilePool } from '../../src/js/tile-pool';
 import { expect } from 'chai';
 import 'mocha';
@@ -23,6 +24,31 @@ describe("TilePool behavior", () => {
                 expect(() => {
                     new TilePool(puzzleData.totalNumberOfTiles, puzzleData.tiles);
                 }).to.throw(Error, "Number of tiles provided (0) does not match number expected (64)!");
+            });
+        });
+
+    });
+
+    describe("selecting a random tile from the TilePool", () => {
+
+        context("when there are tiles remaining", () => {
+            it("should return a tile", () => {
+                const puzzleData = valid_config1.testPuzzleData;
+                const tilePool = new TilePool(puzzleData.totalNumberOfTiles, puzzleData.tiles);
+                const tile = tilePool.getRandomTile();
+                expect(tile).to.be.an.instanceOf(Tile);
+            });
+        });
+
+        context("when there are no more tiles remaining", () => {
+            it("should return null", () => {
+                const puzzleData = valid_config1.testPuzzleData;
+                const tilePool = new TilePool(puzzleData.totalNumberOfTiles, puzzleData.tiles);
+                for (let i = 0; i < puzzleData.totalNumberOfTiles; i++) {
+                    tilePool.getRandomTile();
+                }
+                const tile = tilePool.getRandomTile();
+                expect(tile).to.be.null;
             });
         });
 
