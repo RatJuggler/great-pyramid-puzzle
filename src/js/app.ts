b0c2fe01307f3598fe317afb12d3a8a7c66d2d01
John Chase <ratteal@gmail.com>
2020-06-23 18:45:32 +0100

    Include a copy of the test puzzle in the main app.

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,9 +1,17 @@
+import * as testdata from "../test-puzzle-data.json";
 import * as pocketdata from "../pocket-puzzle-data.json";
 import * as greatdata from "../great-puzzle-data.json";
 import { Tetrahedron } from "./tetrahedron";
 import { TilePool } from "./tile-pool";
 
 console.log("Initialising...");
+
+let test = new Tetrahedron(testdata.puzzle, testdata.numberOfTilesPerFace, testdata.faces);
+console.log(test.toString());
+let testTiles = new TilePool(testdata.totalNumberOfTiles, testdata.tiles);
+console.log(testTiles.toString());
+
+console.log("-------------------------")
 
 let pocket = new Tetrahedron(pocketdata.puzzle, pocketdata.numberOfTilesPerFace, pocketdata.faces);
 console.log(pocket.toString());
