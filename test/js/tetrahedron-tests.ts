d7702caf10318f15600ff0768cf744bfc5a7eb61
John Chase <ratteal@gmail.com>
2020-06-24 09:27:44 +0100

    Start to look at behaviour for randomly adding tiles (without matching).

--- test/js/tetrahedron-tests.ts
+++ test/js/tetrahedron-tests.ts
@@ -14,7 +14,16 @@ describe("Tetrahedron behaviour", function () {
                 const puzzleData = valid_config1.testPuzzleData;
                 const tetrahedron = new Tetrahedron(puzzleData.puzzle, puzzleData.numberOfTilesPerFace, puzzleData.faces);
                 expect(tetrahedron).to.be.an.instanceOf(Tetrahedron);
-                expect(tetrahedron.name).to.equal("test-valid");
+                let expectedToString = "Puzzle Type: test-valid\n" +
+                    "Face: 1, Tile Positions: 1, Joins: (1-A->3-B)(1-B->4-B)(1-C->2-B)\n" +
+                    "Tile: 1, Joins: (1-A->3-1-B)(1-B->4-1-B)(1-C->2-1-B)\n" +
+                    "Face: 2, Tile Positions: 1, Joins: (2-A->3-C)(2-B->1-C)(2-C->4-A)\n" +
+                    "Tile: 1, Joins: (1-A->3-1-C)(1-B->1-1-C)(1-C->4-1-A)\n" +
+                    "Face: 3, Tile Positions: 1, Joins: (3-A->4-C)(3-B->1-A)(3-C->2-A)\n" +
+                    "Tile: 1, Joins: (1-A->4-1-C)(1-B->1-1-A)(1-C->2-1-A)\n" +
+                    "Face: 4, Tile Positions: 1, Joins: (4-A->2-C)(4-B->1-B)(4-C->3-A)\n" +
+                    "Tile: 1, Joins: (1-A->2-1-C)(1-B->1-1-B)(1-C->3-1-A)\n";
+                expect(tetrahedron.toString()).to.equal(expectedToString);
             });
         });
 
@@ -29,22 +38,22 @@ describe("Tetrahedron behaviour", function () {
 
     });
 
-    describe("if a Tile is placed on a Tetrahedron (without using matching)", function () {
+    describe("if #addTileWithoutMatching() is called to place a Tile (without using matching)", function () {
 
         context("and the Tetrahedron has no Tiles on it", function () {
-            it("should be placed in a random position", function () {
+            it("should place it in a random position and return True", function () {
 
             });
         });
 
         context("and the Tetrahedron already has Tiles on it", function () {
-            it("should be placed in a random empty position", function () {
+            it("should place it in a random empty position and return True", function () {
 
             });
         });
 
         context("and the Tetrahedron has no remaining empty positions", function () {
-            it("should return undefined", function () {
+            it("should return False", function () {
 
             });
         });
