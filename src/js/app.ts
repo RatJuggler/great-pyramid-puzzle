f72a263640c80496bdb4a018ef94097e99ffa62d
John Chase <ratteal@gmail.com>
2020-06-24 19:09:42 +0100

    Try loading all tiles on each tetrahedron.

--- src/js/app.ts
+++ src/js/app.ts
@@ -3,24 +3,33 @@ import * as pocketdata from "../pocket-puzzle-data.json";
 import * as greatdata from "../great-puzzle-data.json";
 import { Tetrahedron } from "./tetrahedron";
 import { TilePool } from "./tile-pool";
+import {assert} from "chai";
+
+function placeAllTiles(tilePool: TilePool, tetrahedron: Tetrahedron): void {
+    let tile = tilePool.randomTile;
+    while (tile) {
+        assert.isTrue(tetrahedron.placeTileWithoutMatching(tile));
+        tile = tilePool.randomTile;
+    }
+}
 
 console.log("Initialising...");
 
-let test = new Tetrahedron(testdata.puzzle, testdata.numberOfTilesPerFace, testdata.faces);
-console.log(test.toString());
+let testTetrahedron = new Tetrahedron(testdata.puzzle, testdata.numberOfTilesPerFace, testdata.faces);
 let testTiles = new TilePool(testdata.totalNumberOfTiles, testdata.tiles);
-console.log(testTiles.toString());
+placeAllTiles(testTiles, testTetrahedron);
+console.log(testTetrahedron.toString());
 
 console.log("-------------------------")
 
-let pocket = new Tetrahedron(pocketdata.puzzle, pocketdata.numberOfTilesPerFace, pocketdata.faces);
-console.log(pocket.toString());
+let pocketTetrahedron = new Tetrahedron(pocketdata.puzzle, pocketdata.numberOfTilesPerFace, pocketdata.faces);
 let pocketTiles = new TilePool(pocketdata.totalNumberOfTiles, pocketdata.tiles);
-console.log(pocketTiles.toString());
+placeAllTiles(pocketTiles, pocketTetrahedron);
+console.log(pocketTetrahedron.toString());
 
 console.log("-------------------------")
 
-let great = new Tetrahedron(greatdata.puzzle, greatdata.numberOfTilesPerFace, greatdata.faces);
-console.log(great.toString());
+let greatTetrahedron = new Tetrahedron(greatdata.puzzle, greatdata.numberOfTilesPerFace, greatdata.faces);
 let greatTiles = new TilePool(greatdata.totalNumberOfTiles, greatdata.tiles);
-console.log(greatTiles.toString());
+placeAllTiles(greatTiles, greatTetrahedron);
+console.log(greatTetrahedron.toString());
