6f599e657697fa0a3a86db7161b4f9ee549adade
John Chase <ratteal@gmail.com>
2020-06-25 18:41:59 +0100

    Very simple test using SVG.js to draw.

--- src/js/app.ts
+++ src/js/app.ts
@@ -4,22 +4,26 @@ import greatdata from "../great-puzzle-data.json";
 import { PuzzleData } from "./puzzle-data-schema";
 import { Tetrahedron } from "./tetrahedron";
 import { TilePool } from "./tile-pool";
+import { SVG } from "@svgdotjs/svg.js";
 
-
-function placeAllTiles(tilePool: TilePool, tetrahedron: Tetrahedron): void {
-    let tile = tilePool.randomTile;
-    while (tile) {
-        console.assert(tetrahedron.placeTileWithoutMatching(tile));
-        tile = tilePool.randomTile;
-    }
-}
 
 function loadPuzzleAndPlaceTiles(puzzleType: PuzzleData): void {
-    let tetrahedron = new Tetrahedron(puzzleType.puzzle, puzzleType.numberOfTilesPerFace, puzzleType.faces);
-    let tiles = new TilePool(puzzleType.totalNumberOfTiles, puzzleType.tiles);
-    placeAllTiles(tiles, tetrahedron);
+    const tetrahedron = new Tetrahedron(puzzleType.puzzle, puzzleType.numberOfTilesPerFace, puzzleType.faces);
+    const tiles = new TilePool(puzzleType.totalNumberOfTiles, puzzleType.tiles);
+    let tile = tiles.randomTile;
+    while (tile) {
+        console.assert(tetrahedron.placeTileWithoutMatching(tile));
+        tile = tiles.randomTile;
+    }
     console.log(tetrahedron.toString());
 }
+
+
+const canvas = SVG().addTo("body").size("100%", "100%");
+const path = canvas.path("M 73.9224 -42.6791L -73.9224 -42.6791L 0 85.3583L 73.9224 -42.6791 Z");
+
+path.fill('none').stroke({width: 2, color: '#000'}).move(10,10).scale(0.5);
+path.animate(5000).rotate(365).loop();
 
 
 loadPuzzleAndPlaceTiles(testdata);
