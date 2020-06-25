964ee03d1d1c08ce65128be469b8e43677bd97c8
John Chase <ratteal@gmail.com>
2020-06-25 11:52:57 +0100

    Tidy/refactor.

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,8 +1,10 @@
 import * as testdata from "../test-puzzle-data.json";
 import * as pocketdata from "../pocket-puzzle-data.json";
 import * as greatdata from "../great-puzzle-data.json";
+import { PuzzleData } from "./puzzle-data-schema";
 import { Tetrahedron } from "./tetrahedron";
 import { TilePool } from "./tile-pool";
+
 
 function placeAllTiles(tilePool: TilePool, tetrahedron: Tetrahedron): void {
     let tile = tilePool.randomTile;
@@ -12,23 +14,20 @@ function placeAllTiles(tilePool: TilePool, tetrahedron: Tetrahedron): void {
     }
 }
 
-console.log("Initialising...");
+function loadPuzzleAndPlaceTiles(puzzleType: PuzzleData): void {
+    let tetrahedron = new Tetrahedron(puzzleType.puzzle, puzzleType.numberOfTilesPerFace, puzzleType.faces);
+    let tiles = new TilePool(puzzleType.totalNumberOfTiles, puzzleType.tiles);
+    placeAllTiles(tiles, tetrahedron);
+    console.log(tetrahedron.toString());
+}
 
-let testTetrahedron = new Tetrahedron(testdata.puzzle, testdata.numberOfTilesPerFace, testdata.faces);
-let testTiles = new TilePool(testdata.totalNumberOfTiles, testdata.tiles);
-placeAllTiles(testTiles, testTetrahedron);
-console.log(testTetrahedron.toString());
 
-console.log("-------------------------")
+loadPuzzleAndPlaceTiles(testdata);
 
-let pocketTetrahedron = new Tetrahedron(pocketdata.puzzle, pocketdata.numberOfTilesPerFace, pocketdata.faces);
-let pocketTiles = new TilePool(pocketdata.totalNumberOfTiles, pocketdata.tiles);
-placeAllTiles(pocketTiles, pocketTetrahedron);
-console.log(pocketTetrahedron.toString());
+console.log("-".repeat(80)+'\n');
 
-console.log("-------------------------")
+loadPuzzleAndPlaceTiles(pocketdata);
 
-let greatTetrahedron = new Tetrahedron(greatdata.puzzle, greatdata.numberOfTilesPerFace, greatdata.faces);
-let greatTiles = new TilePool(greatdata.totalNumberOfTiles, greatdata.tiles);
-placeAllTiles(greatTiles, greatTetrahedron);
-console.log(greatTetrahedron.toString());
+console.log("-".repeat(80)+'\n');
+
+loadPuzzleAndPlaceTiles(greatdata);
