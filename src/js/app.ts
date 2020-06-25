4d69b9737b95572684c7b7dd8810cb5dd0490a54
John Chase <ratteal@gmail.com>
2020-06-25 22:24:43 +0100

    Reusing more diagram svg elements.

--- src/js/app.ts
+++ src/js/app.ts
@@ -4,7 +4,7 @@ import greatdata from "../great-puzzle-data.json";
 import { PuzzleData } from "./puzzle-data-schema";
 import { Tetrahedron } from "./tetrahedron";
 import { TilePool } from "./tile-pool";
-import { SVG } from "@svgdotjs/svg.js";
+import { SVG, Matrix } from "@svgdotjs/svg.js";
 
 
 function loadPuzzleAndPlaceTiles(puzzleType: PuzzleData): void {
@@ -19,17 +19,19 @@ function loadPuzzleAndPlaceTiles(puzzleType: PuzzleData): void {
 }
 
 
-const canvas = SVG().addTo("body").size("100%", "100%");
-//<g transform='matrix(0.996264 0 0 0.996264 208.204 228.439)'>
+const canvas = SVG().addTo("body").size("100%", "100%").viewbox("56.4094 53.8583 303.589 261.996");
 const face1 = canvas.path('M  73.9224  -42.6791 L -73.9224  -42.6791 L    0        85.3583 L  73.9224  -42.6791 Z');
 const face2 = canvas.path('M  73.9224  -46.947  L   0      -174.984  L  -73.9224  -46.947  L  73.9224  -46.947  Z');
 const face3 = canvas.path('M 152.113    87.4922 L  78.1903  -40.5452 L    4.26791  87.4922 L 152.113    87.4922 Z');
 const face4 = canvas.path('M  -4.26791  87.4922 L -78.1903  -40.5452 L -152.113    87.4922 L  -4.26791  87.4922 Z');
-
-face1.fill('none').stroke({width: 2, color: '#000'}).move(200,200);
-face2.fill('none').stroke({width: 2, color: '#000'}).move(200,200);
-face3.fill('none').stroke({width: 2, color: '#000'}).move(200,200);
-face4.fill('none').stroke({width: 2, color: '#000'}).move(200,200);
+const tetrahedron = canvas.group();
+tetrahedron.add(face1);
+tetrahedron.add(face2);
+tetrahedron.add(face3);
+tetrahedron.add(face4);
+const matrix = new Matrix(0.996264, 0, 0, 0.996264, 208.204, 228.439);
+tetrahedron.transform(matrix);
+tetrahedron.fill('none').stroke({width: 1, color: '#000'});
 
 
 loadPuzzleAndPlaceTiles(testdata);
