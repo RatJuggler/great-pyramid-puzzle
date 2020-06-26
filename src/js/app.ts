a30fed00f29feaba6947ad65b9e21f0d85efae04
John Chase <ratteal@gmail.com>
2020-06-26 18:09:19 +0100

    Trying to place the tile positions properly.

--- src/js/app.ts
+++ src/js/app.ts
@@ -45,63 +45,65 @@ const tetrahedronTest = [
         }
     ]
 
-// 0.393060796608386       0.2375  0.45
+// 0.393061       0.2375  0.45
 
 const tetrahedronPocket = [
     {
         name: 1,
         center: {x: 0, y: 0, r: 60, s: 1},
         tilePositions: [
-            {x: 0, y: 0, r: 60, s: 0.425},
-            {x: 0, y: 0, r: 60, s: 0.425},
+            {x: 39.3061, y: -23.75, r: 60, s: 0.425},
+            {x: -39.3061, y: -23.75, r: 60, s: 0.425},
             {x: 0, y: 0, r: 0, s: 0.425},
-            {x: 0, y: 0, r: 60, s: 0.425}
+            {x: 0, y: 45, r: 60, s: 0.425}
         ]
     },
     {
         name: 2,
         center: {x: 0, y: -29.8754, r: 0, s: 1},
         tilePositions: [
-            {x: 0, y: 0, r: 0, s: 0.425},
-            {x: 0, y: 0, r: 0, s: 0.425},
+            {x: 0, y: -45, r: 0, s: 0.425},
+            {x: -39.3061, y: 23.75, r: 0, s: 0.425},
             {x: 0, y: 0, r: 60, s: 0.425},
-            {x: 0, y: 0, r: 0, s: 0.425}
+            {x: 39.3061, y: 23.75, r: 0, s: 0.425}
         ]
     },
     {
         name: 3,
         center: {x: 26.0634, y: 14.9377, r: 0, s: 1},
         tilePositions: [
-            {x: 0, y: 0, r: 0, s: 0.425},
-            {x: 0, y: 0, r: 0, s: 0.425},
+            {x: 0, y: -45, r: 0, s: 0.425},
+            {x: -39.3061, y: 23.75, r: 0, s: 0.425},
             {x: 0, y: 0, r: 60, s: 0.425},
-            {x: 0, y: 0, r: 0, s: 0.425}
+            {x: 39.3061, y: 23.75, r: 0, s: 0.425}
         ]
     },
     {
         name: 4,
         center: {x: -26.0634, y: 14.9377, r: 0, s: 1},
         tilePositions: [
-            {x: 0, y: 0, r: 0, s: 0.425},
-            {x: 0, y: 0, r: 0, s: 0.425},
+            {x: 0, y: -45, r: 0, s: 0.425},
+            {x: -39.3061, y: 23.75, r: 0, s: 0.425},
             {x: 0, y: 0, r: 60, s: 0.425},
-            {x: 0, y: 0, r: 0, s: 0.425}
+            {x: 39.3061, y: 23.75, r: 0, s: 0.425}
         ]
     }
 ]
 
-function drawTilePosition(fCenter: CenterPoint, tpCenter: CenterPoint) {
-    const tTilePosition = new Matrix(fCenter.s * tpCenter.s, 0, 0, fCenter.s * tpCenter.s, fCenter.x + tpCenter.x, fCenter.y + tpCenter.y);
-    const tilePosition = canvas.path(unit_triangle).transform(tTilePosition).rotate(tpCenter.r, fCenter.x + tpCenter.x, fCenter.y + tpCenter.y);
+function drawTilePosition(fCenter: CenterPoint, tpData: CenterPoint) {
+    const tpScale = tpData.s * fCenter.s;
+    const tpCenter = {x: fCenter.x + tpData.x, y: fCenter.y + tpData.y, r: tpData.r, s: tpScale};
+    const tTilePosition = new Matrix(tpScale, 0, 0, tpScale, tpCenter.x, tpCenter.y);
+    const tilePosition = canvas.path(unit_triangle).transform(tTilePosition); //.rotate(tpCenter.r, tpCenter.x, tpCenter.y);
     tilePosition.fill('#e6e6e6').stroke(black_line);
 }
 
-function drawFace(faceData: {center: CenterPoint, tilePositions: CenterPoint[]}) {
-    const fScale = faceData.center.s * 3;
-    const fCenter = {x: faceData.center.x * fScale, y: faceData.center.y * fScale, r: faceData.center.r, s: fScale};
+function drawFace(fData: {center: CenterPoint, tilePositions: CenterPoint[]}) {
+    const fScale = fData.center.s * 3;
+    const fCenter = {x: fData.center.x * fScale, y: fData.center.y * fScale, r: fData.center.r, s: fScale};
     const tFace = new Matrix(fScale, 0, 0, fScale, fCenter.x, fCenter.y);
-    const face = canvas.path(unit_triangle).transform(tFace).rotate(faceData.center.r, fCenter.x, fCenter.y);
-    faceData.tilePositions.forEach((tpCenter) => drawTilePosition(fCenter, tpCenter));
+    const face = canvas.path(unit_triangle).transform(tFace).rotate(fData.center.r, fCenter.x, fCenter.y);
+    fData.tilePositions.forEach((tpData) => drawTilePosition(fCenter, tpData));
     face.fill('#f3f3f3').stroke(black_line);
     canvas.circle(1).translate(fCenter.x, fCenter.y).fill('#000').stroke(black_line);
 }
