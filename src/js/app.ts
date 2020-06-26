c57baa8ee5bb67b1d3d0eebfc01e3736fa36dc1e
John Chase <ratteal@gmail.com>
2020-06-26 19:51:19 +0100

    Refactor out duplicated face data and try and alternate way to do rotations.

--- src/js/app.ts
+++ src/js/app.ts
@@ -3,17 +3,18 @@ import {SVG, Matrix, Path} from "@svgdotjs/svg.js";
 
 // dumpPuzzles();
 
-const unit_triangle = 'M 24.6408 14.2264 L 0 -28.4528 L -24.6408 14.2264 L 24.6408 14.2264 Z';
+const up_triangle = 'M 24.6408 14.2264 L 0 -28.4528 L -24.6408 14.2264 L 24.6408 14.2264 Z';
+const down_triangle = 'M -24.6408 -14.2264 L 0 28.4528 L 24.6408 -14.2264 L -24.6408 -14.2264 Z';
 
 const black_line = {width: 0.25, color: '#000000'};
 
-interface CenterPoint {
+interface TriangleData {
     readonly x: number,
     readonly y: number,
     readonly r: number,
 }
 
-const tetrahedronTest = {
+const tetrahedron = {
     faceScale: 1,
     faces: [
         {
@@ -32,34 +33,18 @@ const tetrahedronTest = {
             name: 4,
             center: {x: -26.0634, y: 14.9377, r: 0}
         }
-    ],
+    ]
+}
+
+const testPuzzle = {
     tilePositionScale: 0.9,
     tilePositions: [
         {x: 0, y: 0, r: 0}
     ]
 }
 
-const tetrahedronPocket = {
-    faceScale: 1,
-    faces: [
-        {
-            name: 1,
-            center: {x: 0, y: 0, r: 60}
-        },
-        {
-            name: 2,
-            center: {x: 0, y: -29.8754, r: 0}
-        },
-        {
-            name: 3,
-            center: {x: 26.0634, y: 14.9377, r: 0}
-        },
-        {
-            name: 4,
-            center: {x: -26.0634, y: 14.9377, r: 0}
-        }
-    ],
-    tilePositionScale: 0.425,
+const pocketPuzzle = {
+    tilePositionScale: 0.46,
     tilePositions: [
         {x: 0, y: -45, r: 0},
         {x: -39.3061, y: 23.75, r: 0},
@@ -68,18 +53,18 @@ const tetrahedronPocket = {
     ]
 }
 
-function createTilePosition(fCenter: CenterPoint, tpData: CenterPoint, tpScale: number): Path {
-    const tpCenter = {x: fCenter.x + tpData.x, y: fCenter.y + tpData.y, r: tpData.r};
-    const tTilePosition = new Matrix(tpScale, 0, 0, tpScale, tpCenter.x, tpCenter.y);
-    return canvas.path(unit_triangle).transform(tTilePosition).fill('#e6e6e6').stroke(black_line);
+function createTilePosition(fCenter: TriangleData, tpData: TriangleData, tpScale: number): Path {
+    const tTilePosition = new Matrix(tpScale, 0, 0, tpScale, fCenter.x + tpData.x, fCenter.y + tpData.y);
+    return canvas.path(tpData.r === 0 ? up_triangle : down_triangle).transform(tTilePosition)
+        .fill('#e6e6e6').stroke(black_line);
 }
 
-function drawFace(fData: CenterPoint, faceScale: number, tilePositions: CenterPoint[], tpScale: number) {
+function drawFace(fData: TriangleData, faceScale: number, tilePositions: TriangleData[], tpScale: number) {
     const fScale = faceScale * 3;
     const fCenter = {x: fData.x * fScale, y: fData.y * fScale, r: fData.r};
     const tFace = new Matrix(fScale, 0, 0, fScale, fCenter.x, fCenter.y);
     const face = canvas.group();
-    face.path(unit_triangle).transform(tFace).fill('#f3f3f3').stroke(black_line);
+    face.path(up_triangle).transform(tFace).fill('#f3f3f3').stroke(black_line);
     tilePositions.forEach((tpData) => face.add(createTilePosition(fCenter, tpData, fScale * tpScale)));
     face.rotate(fData.r, fCenter.x, fCenter.y);
     canvas.circle(1).translate(fCenter.x, fCenter.y).fill('#000').stroke(black_line);
@@ -87,7 +72,7 @@ function drawFace(fData: CenterPoint, faceScale: number, tilePositions: CenterPo
 
 const canvas = SVG().addTo("body").size("100%", "100%").viewbox("-200 -200 400 400");
 
-// tetrahedronTest.faces.forEach((face) => drawFace(face.center, tetrahedronTest.faceScale,
-//     tetrahedronTest.tilePositions, tetrahedronTest.tilePositionScale));
-tetrahedronPocket.faces.forEach((face) => drawFace(face.center, tetrahedronPocket.faceScale,
-    tetrahedronPocket.tilePositions, tetrahedronPocket.tilePositionScale));
+// tetrahedron.faces.forEach((face) => drawFace(face.center, tetrahedron.faceScale,
+//     testPuzzle.tilePositions, testPuzzle.tilePositionScale));
+tetrahedron.faces.forEach((face) => drawFace(face.center, tetrahedron.faceScale,
+    pocketPuzzle.tilePositions, pocketPuzzle.tilePositionScale));
