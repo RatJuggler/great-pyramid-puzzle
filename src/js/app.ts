f4ad7ec7c4a10b29d60cacd970d4c7c39f96839a
John Chase <ratteal@gmail.com>
2020-06-26 16:01:39 +0100

    Using only a single triangle definition.

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,43 +1,45 @@
 // import { dumpPuzzles } from "./puzzle-console-dump";
-import { SVG } from "@svgdotjs/svg.js";
+import { SVG, Matrix } from "@svgdotjs/svg.js";
 
 // dumpPuzzles();
 
-const black_line = {width: 1, color: '#000'};
+const unit_triangle = 'M 24.6408 14.2264 L 0 -28.4528 L -24.6408 14.2264 L 24.6408 14.2264 Z';
 
-function drawFace(center: {x: number, y: number}, face: string, tilePosition: string) {
-    canvas.path(face).fill('#f3f3f3').stroke(black_line);
-    canvas.path(tilePosition).fill('#e6e6e6').stroke(black_line);
-    canvas.circle(1).move(center.x, center.y).fill('#000').stroke(black_line);
-}
-
-const canvas = SVG().addTo("body").size("100%", "100%").viewbox("-200 -200 400 400");
+const black_line = {width: 0.25, color: '#000'};
 
 const tetrahedron = [
         {
             name: 1,
             center: {x: 0, y: 0},
-            face: "M  73.9224 -42.6791 L -73.9224  -42.6791 L    0       85.3583 L  73.9224 -42.6791 Z",
-            tilePosition: "M  66.5302 -38.4112 L -66.5302  -38.4112 L    0       76.8224 L  66.5302 -38.4112 Z"
+            rotate: 60
         },
         {
             name: 2,
             center: {x: 0, y: -89.6262},
-            face: "M  73.9224 -46.947  L   0      -174.984  L  -73.9224 -46.947  L  73.9224 -46.947  Z",
-            tilePosition: "M  66.5302 -51.215  L   0      -166.449  L  -66.5302 -51.215  L  66.5302 -51.215  Z"
+            rotate: 0
         },
         {
             name: 3,
             center: {x: 78.1903, y: 44.8131},
-            face: "M 152.113   87.4922 L  78.1903  -40.5452 L    4.2679  87.4922 L 152.113   87.4922 Z",
-            tilePosition: "M 144.721   83.2243 L  78.1903  -32.0094 L   11.6602  83.2243 L 144.721   83.2243 Z"
+            rotate: 0
         },
         {
             name: 4,
             center: {x: -78.1903, y: 44.8131},
-            face: "M  -4.2679  87.4922 L -78.1903  -40.5452 L -152.113   87.4922 L  -4.2679  87.4922 Z",
-            tilePosition: "M -11.6602  83.2243 L -78.1903  -32.0094 L -144.721   83.2243 L -11.6602  83.2243 Z"
+            rotate: 0
         }
     ]
 
-tetrahedron.forEach((face) => drawFace(face.center, face.face, face.tilePosition));
+function drawFace(center: {x: number, y: number}, rotate: number) {
+    const tFace = new Matrix(3, 0, 0, 3, center.x, center.y);
+    const face = canvas.path(unit_triangle).transform(tFace).rotate(rotate, center.x, center.y);
+    face.fill('#f3f3f3').stroke(black_line);
+    const tTilePosition = new Matrix(2.8, 0, 0, 2.8, center.x, center.y);
+    const tilePosition = canvas.path(unit_triangle).transform(tTilePosition).rotate(rotate, center.x, center.y);
+    tilePosition.fill('#e6e6e6').stroke(black_line);
+    canvas.circle(1).translate(center.x, center.y).fill('#000').stroke(black_line);
+}
+
+const canvas = SVG().addTo("body").size("100%", "100%").viewbox("-200 -200 400 400");
+
+tetrahedron.forEach((face) => drawFace(face.center, face.rotate));
