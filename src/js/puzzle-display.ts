2b07113ebdbb43a8ceadf0b895d778666161d678
John Chase <ratteal@gmail.com>
2020-06-28 17:03:44 +0100

    Use an existing SVG node element instead of creating one.

--- src/js/puzzle-display.ts
+++ src/js/puzzle-display.ts
@@ -2,7 +2,7 @@ import display_data from "../display-data.json";
 import { Tile } from "./tile";
 import { Face } from "./face";
 import { Tetrahedron } from "./tetrahedron";
-import { G, Matrix, SVG } from "@svgdotjs/svg.js";
+import {G, Matrix, Svg, SVG} from "@svgdotjs/svg.js";
 
 
 interface TriangleDisplayData {
@@ -24,43 +24,44 @@ interface TileDisplayData {
 
 const black_line = {width: 0.01, color: '#000000'};
 
-const canvas = SVG().addTo("#puzzle-display").size("100%", "100%").viewbox("-50 -100 100 150");
-
-function drawTile(tile: Tile | null, rotate: boolean): G {
-    const tileSegments = canvas.group();
+function drawTile(svg: Svg, tile: Tile | null, rotate: boolean): G {
+    const tileSegments = svg.group();
     if (tile == null) {
         const drawTriangle = rotate ? display_data.down_triangle : display_data.up_triangle;
-        tileSegments.add(canvas.path(drawTriangle).fill('#e6e6e6').stroke(black_line));
+        tileSegments.add(svg.path(drawTriangle).fill('#e6e6e6').stroke(black_line));
     } else {
         const drawSegments = rotate ? display_data.down_segments : display_data.up_segments;
         for (let segN = 0; segN < drawSegments.length; segN++) {
-            tileSegments.add(canvas.path(drawSegments[segN])
+            tileSegments.add(svg.path(drawSegments[segN])
                 .fill(tile.segments.charAt(segN) === '1' ? '#ff0000' : '#ffffff').stroke('none'));
         }
-        tileSegments.add(canvas.circle(0.2).center(0, 0).fill('#bebebe').stroke('none'));
+        tileSegments.add(svg.circle(0.2).center(0, 0).fill('#bebebe').stroke('none'));
     }
     return tileSegments;
 }
 
-function drawFace(fScale: number, fData: TriangleDisplayData, puzzleFace: Face, tileDisplayData: TileDisplayData) {
+function drawFace(svg: Svg, fScale: number, fData: TriangleDisplayData, puzzleFace: Face, tileDisplayData: TileDisplayData) {
     const fCenter = {x: fData.x * fScale, y: fData.y * fScale};
     const fPosition = new Matrix(fScale, 0, 0, fScale, fCenter.x, fCenter.y);
-    const face = canvas.group();
+    const face = svg.group();
     face.path(display_data.up_triangle).transform(fPosition).fill('#f3f3f3').stroke(black_line);
     const tScale = fScale * tileDisplayData.tileScale;
     tileDisplayData.tilePositions.forEach((tpData) => {
         const tile = puzzleFace.getTileAtPosition(tpData.name);
         const tPosition =
             new Matrix(tScale, 0, 0, tScale, (fData.x + tpData.center.x) * fScale, (fData.y + tpData.center.y) * fScale);
-        face.add(drawTile(tile, tpData.center.r === 60).transform(tPosition));
+        face.add(drawTile(svg, tile, tpData.center.r === 60).transform(tPosition));
     });
     face.rotate(fData.r, fCenter.x, fCenter.y);
-    canvas.circle(1).center(fCenter.x, fCenter.y).fill('#000000').stroke(black_line);
+    svg.circle(1).center(fCenter.x, fCenter.y).fill('#000000').stroke(black_line);
 }
 
 function displayPuzzle(puzzleToDisplay: Tetrahedron, tileDisplayData: TileDisplayData) {
+    // Select the existing root SVG element and clear it.
+    const svg = SVG("#puzzle-display") as Svg;
+    svg.clear();
     display_data.faceDisplay.faces.forEach((displayFace) =>
-        drawFace(display_data.faceDisplay.faceScale, displayFace.center, puzzleToDisplay.getFace(displayFace.name), tileDisplayData)
+        drawFace(svg, display_data.faceDisplay.faceScale, displayFace.center, puzzleToDisplay.getFace(displayFace.name), tileDisplayData)
     );
 }
 
