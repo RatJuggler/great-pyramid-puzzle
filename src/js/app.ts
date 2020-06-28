f7ec7b72f91a9069546806881879cbe52d0d4bd2
John Chase <ratteal@gmail.com>
2020-06-28 17:29:13 +0100

    Ignore element warnings.

--- src/js/app.ts
+++ src/js/app.ts
@@ -3,15 +3,15 @@ import {pocketPuzzle} from "./pocket-puzzle";
 import {greatPuzzle} from "./great-puzzle";
 
 
-let testButton = document.getElementById("test");
+let testButton = document.getElementById("test")!;
 testButton.addEventListener("click", () => {
     testPuzzle();
 });
-let pocketButton = document.getElementById("pocket");
+let pocketButton = document.getElementById("pocket")!;
 pocketButton.addEventListener("click", () => {
     pocketPuzzle();
 });
-let greatButton = document.getElementById("great");
+let greatButton = document.getElementById("great")!;
 greatButton.addEventListener("click", () => {
     greatPuzzle();
 });
