ff84e87693b9e31b33bf59bea6d86934539b5007
John Chase <ratteal@gmail.com>
2020-06-28 15:31:03 +0100

    Wire up the buttons to trigger the puzzle displays.

--- src/js/app.ts
+++ src/js/app.ts
@@ -3,6 +3,15 @@ import {pocketPuzzle} from "./pocket-puzzle";
 import {greatPuzzle} from "./great-puzzle";
 
 
-testPuzzle();
-// pocketPuzzle();
-// greatPuzzle();
+let testButton = document.getElementById("test");
+testButton.addEventListener("click", () => {
+    testPuzzle();
+});
+let pocketButton = document.getElementById("pocket");
+pocketButton.addEventListener("click", () => {
+    pocketPuzzle();
+});
+let greatButton = document.getElementById("great");
+greatButton.addEventListener("click", () => {
+    greatPuzzle();
+});
