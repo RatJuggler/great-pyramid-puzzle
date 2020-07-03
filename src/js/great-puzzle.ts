046586f1650cd26f50fbb93af9823b8b934f865d
John Chase <ratteal@gmail.com>
2020-07-03 14:58:32 +0100

    Trying to keep the display data separate.

--- src/js/great-puzzle.ts
+++ src/js/great-puzzle.ts
@@ -1,49 +1,10 @@
-import great_data from "../great-puzzle-data.json";
+import puzzle_data from "../great-puzzle-data.json";
+import display_data from "../great-display-data.json";
 
 
 const greatPuzzle = {
-    puzzleData: great_data,
-    displayData: {
-        tileScale: 0.34,
-        tilePositions: [
-            {
-                name: "1",
-                center: {x: 0, y: -0.333333 * 2, r: 0}
-            },
-            {
-                name: "2",
-                center: {x: -0.288675, y: -0.166667, r: 0}
-            },
-            {
-                name: "3",
-                center: {x: 0, y: -0.333333, r: 60}
-            },
-            {
-                name: "4",
-                center: {x: 0.288675, y: -0.166667, r: 0}
-            },
-            {
-                name: "5",
-                center: {x: -0.288675 * 2, y: 0.333333, r: 0}
-            },
-            {
-                name: "6",
-                center: {x: -0.288675, y: 0.166667, r: 60}
-            },
-            {
-                name: "7",
-                center: {x: 0, y: 0.333333, r: 0}
-            },
-            {
-                name: "8",
-                center: {x: 0.288675, y: 0.166667, r: 60}
-            },
-            {
-                name: "9",
-                center: {x: 0.288675 * 2, y: 0.333333, r: 0}
-            }
-        ]
-    }
+    puzzleData: puzzle_data,
+    displayData: display_data
 }
 
 export { greatPuzzle }
