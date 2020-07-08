da7651ded5c27d72703c7984259d1858a66f0bde
John Chase <ratteal@gmail.com>
2020-07-08 20:35:06 +0100

    Amend to pull in updated data files.

--- src/js/test-puzzle.ts
+++ src/js/test-puzzle.ts
@@ -1,9 +1,11 @@
-import puzzle_data from "../test-puzzle-data.json";
+import layout_data from "../test-layout-data.json";
+import tile_data from "../test-tile-data.json";
 import display_data from "../test-display-data.json";
 
 
 const testPuzzle = {
-    puzzleData: puzzle_data,
+    layoutData: layout_data,
+    tileData: tile_data,
     displayData: display_data
 }
 
