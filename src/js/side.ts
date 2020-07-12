3eb66b5b79f422094ef0fdb7c3ea6ad0e7d3db4c
John Chase <ratteal@gmail.com>
2020-07-12 16:05:11 +0100

    Further refactoring to make better use of Enum.

--- src/js/side.ts
+++ src/js/side.ts
@@ -1,30 +1,28 @@
-enum Side {SideA = 'A', SideB = 'B', SideC = 'C'}
+const enum Side {SideA = 'A', SideB = 'B', SideC = 'C'}
 
 class Sides {
-
-    private static readonly SIDE_NAMES = ['A', 'B', 'C'];
 
     public readonly sideA = Side.SideA;
     public readonly sideB = Side.SideB;
     public readonly sideC = Side.SideC;
 
-    private readonly _sides = new Map<string, Side>();
+    private readonly _sides = new Map<string, Side>([
+        [Side.SideA, Side.SideA],
+        [Side.SideB, Side.SideB],
+        [Side.SideC, Side.SideC]
+    ]);
 
-    constructor() {
-        this._sides.set(this.sideA, this.sideA);
-        this._sides.set(this.sideB, this.sideB);
-        this._sides.set(this.sideC, this.sideC);
-    }
+    constructor() {}
 
     get numberOfSides(): number {
         return this._sides.size;
     }
 
     getSide(side: string, name: string): Side {
-        if (Sides.SIDE_NAMES.includes(side)) {
+        if (this._sides.has(side)) {
             return this._sides.get(side)!;
         }
-        throw new Error(`Side ${name} must be one of ${Sides.SIDE_NAMES}!`);
+        throw new Error(`Side ${name} must be one of ${Side.SideA},${Side.SideB},${Side.SideC}!`);
     }
 
 }
