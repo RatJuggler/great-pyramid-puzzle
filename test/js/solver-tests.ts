3f66352db9832c16459ddecd69b0866162690d99
John Chase <ratteal@gmail.com>
2020-07-22 21:03:20 +0100

    Increase test coverage.

--- test/js/solver-tests.ts
+++ test/js/solver-tests.ts
@@ -219,10 +219,59 @@ describe("BruteForceSolver behaviour", function () {
 
         context("for the first time on an instance instantiated for the test puzzle", function () {
             const result = solver.nextState() as TileChange;
-            it("should return an instance of TileChange for position 1-1 with tile 1", function () {
+            it("should return a Place TileChange for position 1-1 with tile 1", function () {
                 expect(result).to.be.an.instanceOf(TileChange);
+                expect(result.type).to.equal("Place");
                 expect(result.tilePositionId).to.equal("1-1");
                 expect(result.tileId).to.equal(1);
+            });
+        });
+
+        context("for the second time on an instance instantiated for the test puzzle", function () {
+            const result = solver.nextState() as TileChange;
+            it("should return a Place TileChange for position 2-1 with tile 2", function () {
+                expect(result).to.be.an.instanceOf(TileChange);
+                expect(result.type).to.equal("Place");
+                expect(result.tilePositionId).to.equal("2-1");
+                expect(result.tileId).to.equal(2);
+            });
+        });
+
+        context("for the third time on an instance instantiated for the test puzzle", function () {
+            const result = solver.nextState() as TileChange;
+            it("should return a Place TileChange for position 3-1 with tile 3", function () {
+                expect(result).to.be.an.instanceOf(TileChange);
+                expect(result.type).to.equal("Place");
+                expect(result.tilePositionId).to.equal("3-1");
+                expect(result.tileId).to.equal(3);
+            });
+        });
+
+        context("for the fourth time on an instance instantiated for the test puzzle", function () {
+            const result = solver.nextState() as TilePositionChange;
+            it("should return a Rotate TilePositionChange for position 3-1", function () {
+                expect(result).to.be.an.instanceOf(TilePositionChange);
+                expect(result.type).to.equal("Rotate");
+                expect(result.tilePositionId).to.equal("3-1");
+            });
+        });
+
+        context("for the fifth time on an instance instantiated for the test puzzle", function () {
+            const result = solver.nextState() as TilePositionChange;
+            it("should return a Rotate TilePositionChange for position 3-1", function () {
+                expect(result).to.be.an.instanceOf(TilePositionChange);
+                expect(result.type).to.equal("Rotate");
+                expect(result.tilePositionId).to.equal("3-1");
+            });
+        });
+
+        context("for the sixth time on an instance instantiated for the test puzzle", function () {
+            const result = solver.nextState() as TileChange;
+            it("should return a Remove TileChange for position 3-1 with tile 3", function () {
+                expect(result).to.be.an.instanceOf(TileChange);
+                expect(result.type).to.equal("Remove");
+                expect(result.tilePositionId).to.equal("3-1");
+                expect(result.tileId).to.equal(3);
             });
         });
 
