2ff888f5c42610e5ec4eeb8a5687386486939dc7
John Chase <ratteal@gmail.com>
2020-07-23 21:38:01 +0100

    Increase test coverage.

--- test/js/solver-tests.ts
+++ test/js/solver-tests.ts
@@ -149,7 +149,7 @@ describe("NoMatchingSolver behaviour", function () {
             });
         });
 
-        context("with valid placement options", function () {
+        context("with valid sequential placement options", function () {
             const components = getPuzzleComponents(VALID_TEST_PUZZLE);
             const solver = new NoMatchingSolver(components.tetrahedron, components.tilePool,
                 "Sequential", "Sequential", "None");
@@ -158,6 +158,18 @@ describe("NoMatchingSolver behaviour", function () {
             it("should return an updated TilePosition", function () {
                 expect(result).to.be.an.instanceOf(TilePosition);
                 expect(result.id).to.equal("1-1");
+                expect(result.tile).to.eql(tileToPlace);
+            });
+        });
+
+        context("with valid random placement options", function () {
+            const components = getPuzzleComponents(VALID_TEST_PUZZLE);
+            const solver = new NoMatchingSolver(components.tetrahedron, components.tilePool,
+                "Random", "Random", "Random");
+            const tileToPlace = components.tilePool.nextTile!
+            const result = solver.placeTile(tileToPlace);
+            it("should return an updated TilePosition", function () {
+                expect(result).to.be.an.instanceOf(TilePosition);
                 expect(result.tile).to.eql(tileToPlace);
             });
         });
@@ -203,7 +215,7 @@ describe("NoMatchingSolver behaviour", function () {
             });
         });
         context("for the fifth time on an instance instantiated for the test puzzle", function () {
-            const result = solver.nextState() as TileChange;
+            const result = solver.nextState() as PuzzleChange;
             it("should return an instance of PuzzleChange", function () {
                 expect(result).to.be.an.instanceof(PuzzleChange);
             });
@@ -233,7 +245,6 @@ describe("BruteForceSolver behaviour", function () {
                 expect(result.tileId).to.equal(1);
             });
         });
-
         context("for the second time on an instance instantiated for the test puzzle", function () {
             const result = solver.nextState() as TileChange;
             it("should return a Place TileChange for position 2-1 with tile 2", function () {
@@ -243,7 +254,6 @@ describe("BruteForceSolver behaviour", function () {
                 expect(result.tileId).to.equal(2);
             });
         });
-
         context("for the third time on an instance instantiated for the test puzzle", function () {
             const result = solver.nextState() as TileChange;
             it("should return a Place TileChange for position 3-1 with tile 3", function () {
@@ -253,7 +263,6 @@ describe("BruteForceSolver behaviour", function () {
                 expect(result.tileId).to.equal(3);
             });
         });
-
         context("for the fourth time on an instance instantiated for the test puzzle", function () {
             const result = solver.nextState() as TilePositionChange;
             it("should return a Rotate TilePositionChange for position 3-1", function () {
@@ -262,7 +271,6 @@ describe("BruteForceSolver behaviour", function () {
                 expect(result.tilePositionId).to.equal("3-1");
             });
         });
-
         context("for the fifth time on an instance instantiated for the test puzzle", function () {
             const result = solver.nextState() as TilePositionChange;
             it("should return a Rotate TilePositionChange for position 3-1", function () {
@@ -271,7 +279,6 @@ describe("BruteForceSolver behaviour", function () {
                 expect(result.tilePositionId).to.equal("3-1");
             });
         });
-
         context("for the sixth time on an instance instantiated for the test puzzle", function () {
             const result = solver.nextState() as TileChange;
             it("should return a Remove TileChange for position 3-1 with tile 3", function () {
