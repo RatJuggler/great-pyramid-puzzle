60bcac53a0fade14328af5edb558b61324e7d4c7
John Chase <ratteal@gmail.com>
2020-08-02 14:26:03 +0100

    Encapsulate determine options from UI.

---
+++ src/js/ui-options.ts
@@ -0,0 +1,44 @@
+import { SolverOptions } from "./solver-factory";
+
+
+interface UIOptions {
+    displayOption: string;
+    solverOptions: SolverOptions;
+    animationSpeed: number;
+}
+
+class DOMUIOptions implements UIOptions {
+
+    constructor(private readonly _document: Document) {}
+
+    private getSelector(name: string): string {
+        const selection  = this._document.querySelectorAll(`input[name = "${name}"]`) as NodeListOf<HTMLInputElement>;
+        for (const rb of selection) {
+            if (rb.checked) {
+                return rb.value;
+            }
+        }
+        throw new Error("Expected radio option to be selected!");
+    }
+
+    get displayOption(): string {
+        return this.getSelector("display-option");
+    }
+
+    get solverOptions(): SolverOptions {
+        return {
+            puzzleType: this.getSelector("puzzle-type"),
+            solveAlgorithm: this.getSelector("solve-algorithm"),
+            tileSelection: this.getSelector("tile-selection"),
+            tilePlacement: this.getSelector("tile-placement"),
+            tileRotation: this.getSelector("tile-rotation"),
+        };
+    }
+
+    get animationSpeed(): number {
+        return parseInt(this.getSelector("animation-speed"));
+    }
+
+}
+
+export { UIOptions, DOMUIOptions }
