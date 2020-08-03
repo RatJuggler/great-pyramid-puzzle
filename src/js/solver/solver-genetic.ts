448b5a2e2995ae5e225ef1644abe2aa2d4c4e87c
John Chase <ratteal@gmail.com>
2020-08-03 09:47:08 +0100

    A population manager will be required.

--- src/js/solver/solver-genetic.ts
+++ src/js/solver/solver-genetic.ts
@@ -4,10 +4,36 @@ import { Tetrahedron } from "../tetrahedron";
 import { TilePool } from "../tile-pool";
 
 
+class Population {
+
+    private readonly _population = new Array<Tetrahedron>();
+
+    constructor(size: number, tetrahedron: Tetrahedron) {
+        for (let i = 0; i < size; i++) {
+            this._population.push(tetrahedron);
+        }
+    }
+
+    isSolved(): boolean {
+        return true;
+    }
+
+    bestSoFar() {
+        return PuzzleChange.SOLVED;
+    }
+
+}
+
+
 export class GeneticSolver extends SolverBase {
+
+    private static readonly POPULATION_SIZE = 100;
+
+    private readonly _population: Population;
 
     constructor(tetrahedron: Tetrahedron, tilePool: TilePool) {
         super(tetrahedron, tilePool);
+        this._population = new Population(GeneticSolver.POPULATION_SIZE, tetrahedron);
     }
 
     forceNextState(): PuzzleChange {
@@ -15,7 +41,11 @@ export class GeneticSolver extends SolverBase {
     }
 
     nextState(): PuzzleChange {
-        return PuzzleChange.SOLVED;
+        if (this._population.isSolved()) {
+            return PuzzleChange.SOLVED;
+        } else {
+            return this._population.bestSoFar();
+        }
     }
 
 }
