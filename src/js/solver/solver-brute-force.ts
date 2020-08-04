c2cee22b9b723b46a66df6941b77f5699aff1e1d
John Chase <ratteal@gmail.com>
2020-08-04 09:49:59 +0100

    Simplify using map operation.

--- src/js/solver/solver-brute-force.ts
+++ src/js/solver/solver-brute-force.ts
@@ -12,21 +12,18 @@ export class BruteForceSolver extends IterativeSolverBase {
 
     protected createNewState(state: SolverState, newTilePosition: TilePosition): SolverState {
         // Trying every possible tile and rotation combination.
-        const newUntriedTiles = new Array<SolverTileState>();
-        const newRejectedTiles = new Array<SolverTileState>();
         const untriedTiles = state.untriedTiles.concat(state.rejectedTiles);
-        for (const untriedTile of untriedTiles) {
-            const newTileState = {
+        const newUntriedTiles = untriedTiles.map((untriedTile) => {
+            return  {
                 tile: untriedTile.tile,
                 rotations: [0, 1, 2]
             }
-            newUntriedTiles.push(newTileState);
-        }
+        })
         return {
             tilePosition: newTilePosition,
             tileState: null,
             untriedTiles: newUntriedTiles,
-            rejectedTiles: newRejectedTiles
+            rejectedTiles: new Array<SolverTileState>()
         }
     }
 
