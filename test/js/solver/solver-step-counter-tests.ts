0ecf4b197c84454afcc9213059f6cea5da71865a
John Chase <ratteal@gmail.com>
2020-08-06 11:58:25 +0100

    Refactor to use a beforeEach hook.

--- test/js/solver/solver-step-counter-tests.ts
+++ test/js/solver/solver-step-counter-tests.ts
@@ -33,32 +33,32 @@ class MockStatusUpdates implements StatusUpdates {
 
 describe("SolverStepCounter behaviour", function () {
 
+    let mockStatusUpdates: MockStatusUpdates;
+    let stepCounter: SolverStepCounter;
+
+    beforeEach(function () {
+        mockStatusUpdates = new MockStatusUpdates();
+        stepCounter = new SolverStepCounter(mockStatusUpdates);
+    });
+
     describe("if #start() is called", function () {
 
         context("on a newly created instance", function () {
-            const mockStatusUpdates = new MockStatusUpdates();
-            const stepCounter = new SolverStepCounter(mockStatusUpdates);
-            stepCounter.start();
-            it("should set the counter to 0", function () {
+            it("should set the counter to 0 and create a status update", function () {
+                stepCounter.start();
                 expect(stepCounter.counterForDisplay).to.equal("0");
-            });
-            it("should create a status update", function () {
                 const expectedResult = "Add: solver-step-counter - Step Counter - 0";
                 expect(mockStatusUpdates.mockStatus).to.equal(expectedResult);
             });
         });
 
         context("on a instance that has already had it's counter increased", function () {
-            const mockStatusUpdates = new MockStatusUpdates();
-            const stepCounter = new SolverStepCounter(mockStatusUpdates);
-            stepCounter.increase();
-            stepCounter.increase();
-            stepCounter.increase();
-            stepCounter.start();
-            it("should set the counter to 0", function () {
+            it("should set the counter to 0 and create a status update", function () {
+                stepCounter.increase();
+                stepCounter.increase();
+                stepCounter.increase();
+                stepCounter.start();
                 expect(stepCounter.counterForDisplay).to.equal("0");
-            });
-            it("should create a status update", function () {
                 const expectedResult = "Add: solver-step-counter - Step Counter - 0";
                 expect(mockStatusUpdates.mockStatus).to.equal(expectedResult);
             });
@@ -69,28 +69,20 @@ describe("SolverStepCounter behaviour", function () {
     describe("if #increase() is called", function () {
 
         context("on a newly created instance", function () {
-            const mockStatusUpdates = new MockStatusUpdates();
-            const stepCounter = new SolverStepCounter(mockStatusUpdates);
-            stepCounter.increase();
-            it("should increase the counter to 1", function () {
+            it("should increase the counter to 1 and replace the status update", function () {
+                stepCounter.increase();
                 expect(stepCounter.counterForDisplay).to.equal("1");
-            });
-            it("should replace the status update", function () {
                 const expectedResult = "Replace: solver-step-counter - 1";
                 expect(mockStatusUpdates.mockStatus).to.equal(expectedResult);
             });
         });
 
         context("on a instance that has already had it's counter increased twice", function () {
-            const mockStatusUpdates = new MockStatusUpdates();
-            const stepCounter = new SolverStepCounter(mockStatusUpdates);
-            stepCounter.increase();
-            stepCounter.increase();
-            stepCounter.increase();
-            it("should increase the counter to 3", function () {
+            it("should increase the counter to 3 and replace the status update", function () {
+                stepCounter.increase();
+                stepCounter.increase();
+                stepCounter.increase();
                 expect(stepCounter.counterForDisplay).to.equal("3");
-            });
-            it("should replace the status update", function () {
                 const expectedResult = "Replace: solver-step-counter - 3";
                 expect(mockStatusUpdates.mockStatus).to.equal(expectedResult);
             });
@@ -101,28 +93,20 @@ describe("SolverStepCounter behaviour", function () {
     describe("if #setCounter() is called", function () {
 
         context("on a newly created instance", function () {
-            const mockStatusUpdates = new MockStatusUpdates();
-            const stepCounter = new SolverStepCounter(mockStatusUpdates);
-            stepCounter.counter = 1042;
-            it("should set the counter to the value given", function () {
+            it("should set the counter to the value given and replace the status update", function () {
+                stepCounter.counter = 1042;
                 expect(stepCounter.counterForDisplay).to.equal("1,042");
-            });
-            it("should replace the status update", function () {
                 const expectedResult = "Replace: solver-step-counter - 1,042";
                 expect(mockStatusUpdates.mockStatus).to.equal(expectedResult);
             });
         });
 
         context("on a instance that has already had it's counter increased twice", function () {
-            const mockStatusUpdates = new MockStatusUpdates();
-            const stepCounter = new SolverStepCounter(mockStatusUpdates);
-            stepCounter.increase();
-            stepCounter.increase();
-            stepCounter.counter = 1234567;
-            it("should set the counter to the value given", function () {
+            it("should set the counter to the value given and replace the status update", function () {
+                stepCounter.increase();
+                stepCounter.increase();
+                stepCounter.counter = 1234567;
                 expect(stepCounter.counterForDisplay).to.equal("1,234,567");
-            });
-            it("should replace the status update", function () {
                 const expectedResult = "Replace: solver-step-counter - 1,234,567";
                 expect(mockStatusUpdates.mockStatus).to.equal(expectedResult);
             });
