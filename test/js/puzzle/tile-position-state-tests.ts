7ba76ed266049efb139935ed8a6d41882341c020
John Chase <ratteal@gmail.com>
2020-08-06 11:26:41 +0100

    Refactor to use a beforeEach hook.

--- test/js/puzzle/tile-position-state-tests.ts
+++ test/js/puzzle/tile-position-state-tests.ts
@@ -7,10 +7,15 @@ import { TILE_1, TILE_2 } from "../common-test-data";
 
 describe("TilePositionState behaviour", function () {
 
+    let tilePositionState: TilePositionState;
+
+    beforeEach(function () {
+        tilePositionState = new TilePositionState();
+    });
+
     describe("when a new TilePositionState is first created", function () {
 
         context("without setting a Tile", function () {
-            const tilePositionState = new TilePositionState();
             it("should return a correctly initialised instance", function () {
                 expect(tilePositionState).to.be.an.instanceOf(TilePositionState);
             });
@@ -28,15 +33,13 @@ describe("TilePositionState behaviour", function () {
     describe("if #getTile() is called to get the details of a Tile", function () {
 
         context("on a TilePositionState which has a Tile in it", function () {
-            const tilePositionState = new TilePositionState();
-            tilePositionState.tile = TILE_1;
             it("should return the Tile", function () {
+                tilePositionState.tile = TILE_1;
                 expect(tilePositionState.tile).to.equal(TILE_1);
             });
         });
 
         context("on a TilePositionState which doesn't have a Tile in it", function () {
-            const tilePositionState = new TilePositionState();
             it("should return throw an error", function () {
                 expect(function () {
                     tilePositionState.tile;
@@ -49,16 +52,14 @@ describe("TilePositionState behaviour", function () {
     describe("if #isEmpty() is called to test if the TilePositionState already has a Tile", function () {
 
         context("and the TilePositionState is empty", function () {
-            const tilePositionState = new TilePositionState();
             it("should return True", function () {
                 expect(tilePositionState.isEmpty()).to.be.true;
             });
         });
 
         context("and the TilePosition is already occupied by a Tile", function () {
-            const tilePositionState = new TilePositionState();
-            tilePositionState.tile = TILE_1;
             it("should return False", function () {
+                tilePositionState.tile = TILE_1;
                 expect(tilePositionState.isEmpty()).to.be.false;
             });
         });
@@ -68,59 +69,51 @@ describe("TilePositionState behaviour", function () {
     describe("if #setTile() is called to update the TilePositionState with a Tile", function () {
 
         context("and the TilePositionState is empty", function () {
-            const tilePositionState = new TilePositionState();
-            tilePositionState.tile = TILE_1;
-            it("should set the Tile", function () {
+            it("should set the Tile and set the rotation as 0", function () {
+                tilePositionState.tile = TILE_1;
                 expect(tilePositionState.tile).to.equal(TILE_1);
-            });
-            it("and set the rotation as 0", function () {
                 expect(tilePositionState.rotations).to.equal(0);
             })
         });
 
         context("and the TilePositionState already has a Tile", function () {
-            const tilePositionState = new TilePositionState();
-            tilePositionState.tile = TILE_1;
-            it("should throw an error", function () {
+            it("should throw an error and not set the new Tile", function () {
+                tilePositionState.tile = TILE_1;
                 expect(function () {
                     tilePositionState.tile = TILE_2;
                 }).to.throw(Error, "Can't set a Tile when the TilePositionState already has one!");
-            });
-            it("should not set the new Tile", function () {
                 expect(tilePositionState.tile).to.equal(TILE_1);
             });
         });
 
     });
 
-    describe("if #rotations() is called on a TilePositionState", function () {
+    describe("if #setRotations() is called on a TilePositionState", function () {
 
         context("and the TilePositionState doesn't have a Tile", function () {
-            const tilePositionState = new TilePositionState();
-            it("should throw an error", function () {
+            it("should throw an error and not set the new rotations", function () {
                 expect(function () {
                     tilePositionState.rotations = 1;
                 }).to.throw(Error, "No point setting rotations when the TilePositionState doesn't have a Tile!");
-            });
-            it("should not set the new rotations", function () {
                 expect(tilePositionState.rotations).to.equal(0);
             });
         });
 
         context("with an invalid number of rotations", function () {
-            const tilePositionState = new TilePositionState();
-            tilePositionState.tile = TILE_1;
             it("should throw an error", function () {
+                tilePositionState.tile = TILE_1;
                 expect(function () {
                     tilePositionState.rotations = 4;
                 }).to.throw(Error, "Tile can only be rotated 0, 1 or 2 times!");
             });
             it("should throw an error", function () {
+                tilePositionState.tile = TILE_1;
                 expect(function () {
                     tilePositionState.rotations = -3;
                 }).to.throw(Error, "Tile can only be rotated 0, 1 or 2 times!");
             });
             it("should throw an error", function () {
+                tilePositionState.tile = TILE_1;
                 expect(function () {
                     tilePositionState.rotations = 1.5;
                 }).to.throw(Error, "Tile can only be rotated 0, 1 or 2 times!");
@@ -128,10 +121,9 @@ describe("TilePositionState behaviour", function () {
         });
 
         context("with a valid number of rotations", function () {
-            const tilePositionState = new TilePositionState();
-            tilePositionState.tile = TILE_1;
-            tilePositionState.rotations = 2;
             it("should set the rotations", function () {
+                tilePositionState.tile = TILE_1;
+                tilePositionState.rotations = 2;
                 expect(tilePositionState.rotations).to.equal(2);
             });
         });
@@ -141,39 +133,28 @@ describe("TilePositionState behaviour", function () {
     describe("if #rotate() is called on a TilePosition", function () {
 
         context("and the TilePositionState doesn't have a Tile", function () {
-            const tilePositionState = new TilePositionState();
-            it("should throw an error", function () {
+            it("should throw an error and not do a rotation", function () {
                 expect(function () {
                     tilePositionState.rotate();
                 }).to.throw(Error, "No point calling rotate when the TilePositionState doesn't have a Tile!");
-            });
-            it("should not do a rotation", function () {
                 expect(tilePositionState.rotations).to.equal(0);
             });
         });
 
         context("with a newly placed Tile", function () {
-            const tilePositionState = new TilePositionState();
-            tilePositionState.tile = TILE_1;
-            const result = tilePositionState.rotate();
-            it("should return true", function () {
-                expect(result).to.be.true;
-            });
-            it("should track the Tile as being rotated once", function () {
+            it("should return true and track the Tile as being rotated once", function () {
+                tilePositionState.tile = TILE_1;
+                expect(tilePositionState.rotate()).to.be.true;
                 expect(tilePositionState.rotations).to.equal(1);
             });
         });
 
         context("and the Tile has already been rotated twice", function () {
-            const tilePositionState = new TilePositionState();
-            tilePositionState.tile = TILE_1;
-            tilePositionState.rotate();
-            tilePositionState.rotate();
-            const result = tilePositionState.rotate();
-            it("should return false", function () {
-                expect(result).to.be.false;
-            });
-            it("should reset the rotation back to 0", function () {
+            it("should return false and reset the rotation back to 0", function () {
+                tilePositionState.tile = TILE_1;
+                tilePositionState.rotate();
+                tilePositionState.rotate();
+                expect(tilePositionState.rotate()).to.be.false;
                 expect(tilePositionState.rotations).to.equal(0);
             });
         });
@@ -183,7 +164,6 @@ describe("TilePositionState behaviour", function () {
     describe("if #removeTile() is called to remove a Tile", function () {
 
         context("and the TilePosition is empty", function () {
-            const tilePositionState = new TilePositionState();
             it("should throw an error", function () {
                 expect(function () {
                     tilePositionState.removeTile();
@@ -192,14 +172,10 @@ describe("TilePositionState behaviour", function () {
         });
 
         context("and the TilePosition has a Tile in it", function () {
-            const tilePositionState = new TilePositionState();
-            tilePositionState.tile = TILE_1;
-            const result = tilePositionState.removeTile();
-            it("should remove the Tile", function () {
+            it("should return the Tile and remove it", function () {
+                tilePositionState.tile = TILE_1;
+                expect(tilePositionState.removeTile()).to.eql(TILE_1);
                 expect(tilePositionState.isEmpty()).to.be.true;
-            });
-            it("should return Tile", function () {
-                expect(result).to.eql(TILE_1);
             });
         });
 
